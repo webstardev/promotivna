16a60a545d161c1e54d5c566cfaf083003103575
True Code Masters <hlymca84@gmail.com>
2021-05-19 09:24:13 +0530

    Product Crud

---
+++ MarkomPos.Repository/Repository/ProductRepository.cs
@@ -0,0 +1,54 @@
+﻿using MarkomPos.Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkomPos.Repository.Repository
+{
+    public class ProductRepository : IDisposable
+    {
+        public bool AddUpdateProduct(Product product)
+        {
+            using (var context = new markomPosDbContext())
+            {
+                try
+                {
+                    if (product.ID > 0)
+                    {
+                        var dbData = context.Products.Find(product.ID);
+                        if (dbData != null)
+                        {
+                            dbData.ID = product.ID;
+                            dbData.Name = product.Name;
+                            dbData.DisplayName = product.DisplayName;
+                            dbData.Note = product.Note;
+                            dbData.Note2 = product.Note2;
+                            dbData.Code = product.Code;
+                            dbData.UnitOfMeasureId = product.UnitOfMeasureId;
+                            dbData.ProductGroupId = product.ProductGroupId;
+                            dbData.DateModified = DateTime.Now;
+                        }
+                    }
+                    else
+                    {
+                        product.DateCreated = DateTime.Now;
+                        product.DateModified = DateTime.Now;
+                        context.Products.Add(product);
+                    }
+                    context.SaveChanges();
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    return false;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+        }
+    }
+}
---
+++ MarkomPos.Web/MarkomPos.Web/Controllers/ProductsController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MarkomPos.Model.Model;
+using MarkomPos.Repository;
+using MarkomPos.Repository.Repository;
+
+namespace MarkomPos.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private markomPosDbContext db = new markomPosDbContext();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.ProductGroup).Include(p => p.UnitOfMeasure);
+            return View(products.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductGroupId = new SelectList(db.ProductGroups, "ID", "Name");
+            ViewBag.UnitOfMeasureId = new SelectList(db.UnitOfMeasures, "ID", "Name");
+            return PartialView("_Details", product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProductGroupId = new SelectList(db.ProductGroups, "ID", "Name");
+            ViewBag.UnitOfMeasureId = new SelectList(db.UnitOfMeasures, "ID", "Name");
+            return PartialView("_AddProduct", null);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            using (var productRepository = new ProductRepository())
+            {
+                var result = productRepository.AddUpdateProduct(product);
+                if (result)
+                    return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductGroupId = new SelectList(db.ProductGroups, "ID", "Name", product.ProductGroupId);
+            ViewBag.UnitOfMeasureId = new SelectList(db.UnitOfMeasures, "ID", "Name", product.UnitOfMeasureId);
+            return PartialView("_AddProduct", product);
+        }
+
+        [HttpGet, ActionName("DeleteConfirmed")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
